91934366a4914ae1db517cc4843a5e9de3a439a7
Yuri Lira Nunes <yliranunes@gmail.com>
2022-09-11 21:40:55 -0300

    Utilizando Git no Eclipse

---
+++ spring-data/src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -0,0 +1,15 @@
+package br.com.alura.spring.data.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.spring.data.orm.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
+
+	List<Funcionario> findByNome(String nome);
+
+}
